2592b11bc300ef096039560f879fc509453b79a5
ThiagoLopes589 <thiagolopes589@yahoo.com>
2026-05-12 22:44:56 -0300

    Adicionado comparação das cartas com IF ELSE

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
 
 int main(){
+    // Título
+    printf("INICIANDO JOGO: SUPER TRUNFO COM 2 CARTAS\n\n");
+    printf("Regras do jogo: \n");
+    printf("Cada carta representa uma cidade, com atributos como população, área, PIB e número de pontos turísticos.\n");
+    printf("Ao final será comparado os atributos entre as duas cartas, o atributo de maior valor vence (exceto para densidade populacional, onde o menor valor vence).\n\n");
+
+    // Iniciando variaveis calculadas pelo código, como a densidade populacional, o PIB per capta e o super poder da carta 1 e 2
+    float DensidadePopulacional1, PibPerCapta1, superPoder1, DensidadePopulacional2, PibPerCapta2, superPoder2;
+/*
+    // INICIO DA AREA DE TESTES 
+    //Aqui é possível testar o código, inserindo os dados das cartas de forma Hardcoded, ou seja, sem
+    //a necessidade de inserir os dados manualmente. Para isso, basta descomentar a área de 
+    //testes e comentar a área de coleta dos dados das cartas. 
+    //(Além desta área, o código possui a area1 e area2, que devem ser comentadas antes de rodar o teste)
+
+    // Iniciando e declarando variáveis da carta 1 e 2, com dados hardcoded para testes.
+    char estado1[50] = "A", codigo1[50] = "A01", nomedacidade1[50] = "SÃOPAULO";
+    int numerodepontosturisticos1 = 50;
+    unsigned long int populacao1 = 12325000;
+    float area1 = 1521.11, pib1 = 699.28;
+
+    char estado2[50] = "B", codigo2[50] = "B02", nomedacidade2[50] = "RIODEJANEIRO";
+    int numerodepontosturisticos2 = 30;
+    unsigned long int populacao2 = 6748000;
+    float area2 = 1200.25, pib2 = 300.50;
+    //FINAL DA AREA DE TESTES
+*/
+                                //INICIO DA AREA1 DE EXCLUSAO PARA TESTES 
+    
     // Iniciando todas as variáveis necessárias para a descrição da carta 1
     char estado1[50], codigo1[50], nomedacidade1[50];
     int numerodepontosturisticos1;
     unsigned long int populacao1;
-    float area1, pib1, DensidadePopulacional1, PibPerCapta1, superPoder1;
-    
-    // Título
-    printf("INICIANDO JOGO: SUPER TRUNFO COM 2 CARTAS\n\n");
+    float area1, pib1;
+
+    // Iniciando todas as variáveis necessárias para a descrição da carta 2
+    char estado2[50], codigo2[50], nomedacidade2[50];
+    int numerodepontosturisticos2;
+    unsigned long int populacao2;
+    float area2, pib2;
 
     // Início da coleta dos dados da carta 1
     printf("Insira as informações da Carta 1: \n");
@@ -33,10 +65,12 @@ int main(){
     printf("Número de Pontos Turísticos: \n");
     scanf("%d", &numerodepontosturisticos1);
 
-    /* Descrevendo como serão calculadas as novas variáveis1, incluindo o super 
+                                // **FINAL DA AREA1 DE EXCLUSAO PARA TESTES
+
+
+    /*Descrevendo como serão calculadas as novas variáveis1, incluindo o super 
     poder da carta 1, que é a soma do inverso da densidade populacional, do PIB per capta e do
     número de pontos turísticos.*/
-    
     DensidadePopulacional1 = (float) (populacao1 / area1);
     PibPerCapta1 = (float) ((pib1 * 1000000000) / populacao1);
     superPoder1 = (float) ( 1 / DensidadePopulacional1) + PibPerCapta1 + numerodepontosturisticos1 + area1 + populacao1 + pib1;
@@ -55,15 +89,10 @@ int main(){
     printf("PIB per capta: %.2f reais\n", PibPerCapta1);
     printf("Super poder Carta 1: %.2f\n", superPoder1);
 
-
-
-    // Iniciando todas as variáveis necessárias para a descrição da carta 2
-    char estado2[50], codigo2[50], nomedacidade2[50];
-    int numerodepontosturisticos2;
-    unsigned long int populacao2;
-    float area2, pib2, DensidadePopulacional2, PibPerCapta2, superPoder2;
-
+                                // INICIO DA AREA2 DE EXCLUSAO PARA TESTES
+    
     // Início da coleta dos dados da carta 2
+
     printf("\n");
     printf("Insira as informações da Carta 2: \n");
     printf("Estado: \n");
@@ -87,10 +116,11 @@ int main(){
     printf("Número de Pontos Turísticos: \n");
     scanf("%d", &numerodepontosturisticos2);
 
-    /* Descrevendo como serão calculadas as novas variáveis2, incluindo o super 
+                                // **FINAL DA AREA2 DE EXCLUSAO PARA TESTES
+    
+    /*Descrevendo como serão calculadas as novas variáveis2, incluindo o super 
     poder da carta 2, que é a soma do inverso da densidade populacional, do PIB per capta e do
     número de pontos turísticos.*/
-
     DensidadePopulacional2 = (float) (populacao2 / area2);
     PibPerCapta2 = (float) ((pib2 * 1000000000) / populacao2);
     superPoder2 = (float) ( 1 / DensidadePopulacional2) + PibPerCapta2 + numerodepontosturisticos2 + area2 + populacao2 + pib2;
@@ -112,19 +142,62 @@ int main(){
 
     /* Início da comparação entre as cartas 1 e 2, onde é comparado os atributos de 
     cada carta (Exceto estado, código e nome da cidade) e é exibido qual carta tem o 
-    maior valor em cada atributo. 
-    O maior vence, sendo o resultado 1 = ponto para carta1 e 0 = ponto para carta2.*/
+    maior valor em cada atributo (maior vence, exceto densidade populacional).*/
 
-    printf("**Comparação entre as cartas** \n");
-    printf("1 = ponto para Carta 1, 0 = ponto para Carta 2\n\n");
-    printf("População: %d\n", populacao1 > populacao2);
-    printf("Área: %d\n", area1 > area2);
-    printf("PIB: %d\n", pib1 > pib2);
-    printf("Pontos Turísticos: %d\n", numerodepontosturisticos1 > numerodepontosturisticos2);
-    printf("Densidade Populacional: %d\n", DensidadePopulacional1 < DensidadePopulacional2);
-    printf("PIB per Capita: %d\n", PibPerCapta1 > PibPerCapta2);
-    printf("Super Poder: %d\n", superPoder1 > superPoder2);
+    printf("**Resultado final da comparação entre as cartas**\n");
     
+    printf("Comparando a população: ");
+    if(populacao1 > populacao2) {
+        printf("%s venceu!\n", nomedacidade1);
+    }   else   {
+        printf("%s venceu!", nomedacidade2);
+    }
+    
+    printf("Comparando a área: ");
+    if(area1 > area2) {
+        printf("%s venceu!\n", nomedacidade1);
+    }   else   {
+        printf("%s venceu!\n", nomedacidade2);
+    }
+     
+    printf("Comparando o PIB: ");
+    if(pib1 > pib2) {
+        printf("%s venceu!\n", nomedacidade1);
+    }   else   {
+        printf("%s venceu!\n", nomedacidade2);
+    }
+     
+    printf("Comparando o número de pontos turísticos: ");
+    if(numerodepontosturisticos1 > numerodepontosturisticos2) {
+        printf("%s venceu!\n", nomedacidade1);
+    }   else   {
+        printf("%s venceu!\n", nomedacidade2);
+    }
+     
+    printf("Comparando a densidade populacional: ");
+    if(DensidadePopulacional1 < DensidadePopulacional2) {
+        printf("%s venceu!\n", nomedacidade1);
+    }   else   {
+        printf("%s venceu!\n", nomedacidade2);
+    }
+       
+    printf("Comparando o PIB per capta: ");
+    if(PibPerCapta1 > PibPerCapta2) {
+        printf("%s venceu!\n", nomedacidade1);
+    }   else   {
+        printf("%s venceu!\n", nomedacidade2);
+    } 
+
+    printf("Comparando o super poder: ");
+    if(superPoder1 > superPoder2) {
+        printf("%s venceu!\n", nomedacidade1);
+    }   else   {
+        printf("%s venceu!\n", nomedacidade2);
+    };
+
+
+
+
 
     return 0;
 }
